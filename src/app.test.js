3e031dffc90b4043c9b1d583b3cffd4b48cae97b
Barış Gündüzer <barisgunduzer@users.noreply.github.com>
2022-01-31 17:09:59 +0300

    this should break it

--- src/app.test.js
+++ src/app.test.js
@@ -1,6 +1,7 @@
-import { dayOfTheWeek } from "./app";
+const { dayOfTheWeek } = require('./app');
 
-test("getDay returns the long-format day of the week", () => {
-  const day = dayOfTheWeek(new Date("3/11/2020"));
-  expect(day).toBe("Wednesday");
+
+test('getDay returns the long-format day of the week', () => {
+    const day = dayOfTheWeek( new Date('3/11/2020') );
+    expect( day ).toBe('Wednesday');
 });
